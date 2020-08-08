8d8ae3c0505be7b609da350f4a4d8d1a9f51b308
sarita87das <nutankarna87@gmail.com>
2020-08-07 22:14:12 -0400

    first commit on employee directory

---
+++ reactpractice/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
+import SearchForm from "./components/SearchForm";
+import ResultList from "./components/ResultList";
+import EmployeeCard from "./components/EmployeeCard";
+import axios from "axios";
+function App() {
+  return <SearchResultContainer />;
+}
+
+export default App;
