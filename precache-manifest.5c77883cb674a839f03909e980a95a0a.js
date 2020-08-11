61ffff8f327187f551eba404e8fbc67811ea9237
sarita87das <nutankarna87@gmail.com>
2020-08-10 20:25:29 -0400

    Updates

---
+++ precache-manifest.5c77883cb674a839f03909e980a95a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaf0c602e37380a316fb3d8d891103f",
+    "url": "/sarita87das/Employee_Directory/index.html"
+  },
+  {
+    "revision": "7290bc0607a1101a6d42",
+    "url": "/sarita87das/Employee_Directory/static/css/main.0cb898ac.chunk.css"
+  },
+  {
+    "revision": "2e8577f6067b8e14d16b",
+    "url": "/sarita87das/Employee_Directory/static/js/2.c1e8bf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sarita87das/Employee_Directory/static/js/2.c1e8bf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7290bc0607a1101a6d42",
+    "url": "/sarita87das/Employee_Directory/static/js/main.62389f98.chunk.js"
+  },
+  {
+    "revision": "d518da96a80312e78386",
+    "url": "/sarita87das/Employee_Directory/static/js/runtime-main.7da62da7.js"
+  }
+]);
